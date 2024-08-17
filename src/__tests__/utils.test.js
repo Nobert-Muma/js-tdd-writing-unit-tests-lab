c3e0639c1a9117acb099f7826c5c2ae6af84f163
Nobert-Muma <mumanobert17@gmail.com>
2024-08-17 12:21:46 +0300

    Wrote a test for checking if a word is a palindrome

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,10 @@
 // Your tests here
+// implement a palindrome word that reads the same forwards and backwards
+import isPalindrome from "../utils";
+describe("isPalindrome", ()=>{
+    it("accepts a string as an argument and returns true if the string is a palindrome and false if it isn't", ()=>{
+        let keyedInString="radar";
+        let palindromeValidator=isPalindrome(keyedInString);
+        expect(palindromeValidator).toBe(true);
+    })
+})
