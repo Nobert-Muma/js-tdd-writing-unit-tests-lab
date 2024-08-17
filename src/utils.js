ddee21dcd0c136c2049f646679ebd31117f6da0c
Nobert-Muma <mumanobert17@gmail.com>
2024-08-17 12:20:15 +0300

    Wrote code to pass the palindrome test

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,10 @@
 // Your code here
+function isPalindrome(stringToTest){
+    let reversedString=stringToTest.split("").reverse().join("");
+    if(stringToTest===reversedString){
+        return true;
+    } else {
+        return false;
+    }
+}
+export default isPalindrome;
